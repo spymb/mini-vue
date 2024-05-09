3ada42cbd7fd5b300744dea3a1ac4acb2ec7ecfd
spym <928489998@qq.com>
2024-05-09 14:10:56 +0800

    实现reactive和effect，依赖收集及触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,44 @@
+class ReactiveEffect {
+  private _fn: any;
+
+  constructor(fn) {
+    this._fn = fn;
+  }
+
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+let activeEffect;
+export const effect = (fn) => {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+};
+
+const targetMap = new Map();
+export const track = (target, key) => {
+  // target -> [key -> deps]
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let deps = depsMap.get(key);
+  if (!deps) {
+    deps = new Set();
+    depsMap.set(key, deps);
+  }
+
+  deps.add(activeEffect);
+};
+
+export const trigger = (target, key) => {
+  const depsMap = targetMap.get(target);
+  const deps = depsMap.get(key);
+  for (const dep of deps) {
+    dep.run();
+  }
+};
